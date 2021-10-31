04f0770539d54a25aed33a3318fef1fdaa01b575
maximilian <maximilianproell@hotmail.com>
2021-10-31 17:39:34 +0100

    initial commit of basic compose app

---
+++ app/src/main/java/com/example/simplepomodoro/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.simplepomodoro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.simplepomodoro.ui.theme.SimplePomodoroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimplePomodoroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SimplePomodoroTheme {
+        Greeting("Android")
+    }
+}
